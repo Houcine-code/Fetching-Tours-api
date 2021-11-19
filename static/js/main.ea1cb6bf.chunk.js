b40b3877a031eb020492adf3d6c3684c6cdda3d1
Houcine-code <houc.bh90@gmail.com>
2021-11-19 21:49:55 +0100

    Updates

--- static/js/main.ea1cb6bf.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpfetching-tours-api"]=this["webpackJsonpfetching-tours-api"]||[]).push([[0],{4:function(t,e,n){"use strict";n.r(e);var c=n(0),i=n.n(c),r=n(3),s=n.n(r),o=(n(9),n(2));s.a.render(Object(o.jsx)(i.a.StrictMode,{children:Object(o.jsx)("h1",{children:"ReactJS"})}),document.getElementById("root"))},9:function(t,e,n){}},[[4,1,2]]]);
-//# sourceMappingURL=main.ea1cb6bf.chunk.js.map
